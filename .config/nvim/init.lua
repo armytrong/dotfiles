a8c4b4adb8750d281f5e9e71bf00b1065cfda336
jakob <j.gierschmann@gmail.com>
2023-03-15 22:33:04 +0100

    added nvim configuration
    added nvim configs icluding autocomplete. TODO: list packets that have
    to be installed.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,32 @@
+require("plugins")
+require("complete")
+-- lualine
+require('lualine').setup{
+    options = { theme = 'powerline' }
+}
+
+
+-- tabs
+vim.o.abstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smarttab = true
+
+
+-- complete
+--require("nvim-lsp-installer").setup({
+--    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+--    ui = {
+--        icons = {
+--            server_installed = "✓",
+--            server_pending = "➜",
+--            server_uninstalled = "✗"
+--        }
+--    }
+--})
+--
+
+local lsp = require("lspconfig")
+lsp.jedi_language_server.setup({on_attach=on_attach})
+lsp.lua_ls.setup({on_attach=on_attach})
+lsp.clangd.setup({on_attach=on_attach})
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,44 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+
+return require('packer').startup(function(use)
+
+-- Packer can manage itself
+use 'wbthomason/packer.nvim'
+
+-- lualine
+use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+}
+
+--completion
+use 'neovim/nvim-lspconfig' --config for Nvim LSP
+use 'hrsh7th/cmp-nvim-lsp'
+use 'hrsh7th/cmp-buffer'
+use 'hrsh7th/cmp-path'
+use 'hrsh7th/cmp-cmdline'
+use 'hrsh7th/nvim-cmp'
+
+use 'hrsh7th/cmp-vsnip'
+use 'hrsh7th/vim-vsnip'
+
+--use 'williamboman/nvim-lsp-installer' -- lsp-installer
+
+-- Automatically set up your configuration after cloning packer.nvim
+if packer_bootstrap then
+    require('packer').sync()
+end
+
+end)
