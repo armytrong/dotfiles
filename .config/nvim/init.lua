dccf2cdd6ee7891dcbcc7a5689bd915f13efd3ed
jakob <j.gierschmann@gmail.com>
2023-05-31 00:31:27 +0200

    repaired vim completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,8 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smarttab = true
 
+vim.o.laststatus = 3
+
 
 -- complete
 
@@ -29,5 +31,18 @@ require("nvim-lsp-installer").setup({
 
 local lsp = require("lspconfig")
 lsp.jedi_language_server.setup({on_attach=on_attach})
-lsp.lua_ls.setup({on_attach=on_attach})
-lsp.clangd.setup({on_attach=on_attach})
+lsp.lua_ls.setup{
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+lsp.clangd.setup{
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+lsp.texlab.setup{
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+
+vim.cmd('set grepprg=grep\\ -nH\\ $*')
+vim.cmd('let g:tex_flavor = "latex"')
